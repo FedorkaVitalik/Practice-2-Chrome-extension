09afc16c7250b65a64c728784b6b45a35885e7a2
Vitalij Fedorka <vitalik@MacBook-Pro-Vitalij.local>
2018-09-06 11:43:13 +0300

    Add comments to file script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-$(window).load(function() {
+$(window).load(function () {
+
+	//api Приватбанку дає інфомацію по курсу гривні до різних валют на поточну дату
 	let course = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode=';
 
+	//Функція, яка вибирає елементи з даним класом і після кожного елемента записує
+	//новий параграф з новою ціною у валюті(після відповіді з сервера)
 	function updatePrices(rate) {
-		$('.auto-large-price, .auto-item-cena b:first-child').each(function(i, el) {
+		$('.auto-large-price, .auto-item-cena b:first-child').each(function (i, el) {
 			let priceInZl = $(this).text();
 			let strToNum = Number(priceInZl.replace(/\D+/g, ''));
 			let priceInUsd = strToNum / rate;
@@ -10,9 +14,12 @@ $(window).load(function() {
 		});
 	}
 
-	$.getJSON(course + 'pln&date&json', function(data) {
+	//Запит на api Приватбанку по конкретній валюті
+	//Після відповіді з серверу викликається функція updatePrices яка за параметр
+	//приймає курс валюти та виконує дії прописані у функції
+	$.getJSON(course + 'pln&date&json', function (data) {
 		let courseHrZl = data[0].rate;
-		$.getJSON(course + 'usd&date&json', function(data) {
+		$.getJSON(course + 'usd&date&json', function (data) {
 			let courseHrUsd = data[0].rate;
 			let ZlToUsd = courseHrUsd / courseHrZl;
 			updatePrices(ZlToUsd);
